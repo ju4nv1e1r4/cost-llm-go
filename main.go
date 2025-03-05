de35e6727b02c5f1ab650ddc838c4f68eb49d670
juanvieira <juanvieira1@yahoo.com>
2025-03-05 09:28:56 -0300

    => clear

--- main.go
+++ main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"llmcosts/app"
-	"os"
-)
-
 func main()  {
-	fmt.Println("=======")
-	fmt.Println("LLM Cost of Trainning and Inputs")
-	fmt.Println("=======")
-	fmt.Print("Qual o nome do seu modelo?\n")
-	var text string
-	fmt.Scanln(&text)
-	fmt.Printf("Modelo => %s", text)
-
-	fmt.Println("\n---------")
-	fmt.Print("Digite um prompt:\n")
-	var input string
-	fmt.Scanln(&input)
-	fmt.Printf("Prompt => %s\n", input)
-
-	fineTuningData, err := os.ReadFile("data.txt")
-	if err != nil {
-		panic(err)
-	}
 	
-	trainningCost := app.CalcTrainning(text, string(fineTuningData))
-	inputCost := app.CalcInput(text, input)
-
-	fmt.Printf("=> O custo do treinamento deste modelo é USD %f.\n", trainningCost)
-	fmt.Printf("=> O custo do prompt deste modelo é USD %f.", inputCost)
 }
