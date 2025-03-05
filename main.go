09808b046694f5d3eb9054578cd223db5ad97419
juanvieira <juanvieira1@yahoo.com>
2025-03-05 09:34:50 -0300

    build: prepare for compile

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"llmcosts/app"
+	"os"
+)
+
 func main()  {
-	
+    fmt.Println("=======")
+	fmt.Println("LLM Cost of Trainning and Inputs")
+	fmt.Println("=======")
+
+	fmt.Print("Qual o nome do seu modelo?\n")
+	var text string
+	fmt.Scanln(&text)
+	fmt.Printf("Modelo => %s", text)
+    fineTuningData, err := os.ReadFile("data.txt")
+    if err != nil {
+		panic(err)
+	}
+
+    app.CalcTrainning(text, string(fineTuningData))
 }
